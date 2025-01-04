2becd2f58c8d5ab0a9a1b4e3cad2fc604c582074
indraagstin025 <indraagustin012@gmail.com>
2025-01-04 16:49:39 +0700

    add requirements and __init__py

--- app/__init__.py
+++ app/__init__.py
@@ -19,9 +19,11 @@ def create_app(config_name='default'):
     app = Flask(__name__)
     
     if config_name == 'testing':
-        app.config.from_object('config.TestingConfig')  
+        app.config.from_object('config.TestingConfig')
+    elif config_name == 'development':
+        app.config.from_object('config.DevelopmentConfig')
     else:
-        app.config.from_object('config.Config') 
+        app.config.from_object('config.ProductionConfig')
     
     init_extensions(app)
     register_blueprints(app)
